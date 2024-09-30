28aa8d78a544d1e7cd900d2c2ce9142f1ec2a70f
Yazan <yazan.dukhan@student.ap.be>
2024-09-30 19:13:32 +0200

    OefeningNamedOptions DOEN!

---
+++ labos/labo05/OefeningNamedOptions/OefeningNamedOptions/Controllers/EmaillSettingsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace OefeningNamedOptions.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmailConfigurationsController : ControllerBase
+    {
+        private readonly IOptionsSnapshot<EmailOptions> _options;
+
+        public EmailConfigurationsController(IOptionsSnapshot<EmailOptions> options)
+        {
+            _options = options;
+        }
+
+        [HttpGet]
+        public IActionResult GetEmailConfigurations()
+        {
+            var gmailOptions = _options.Get(EmailOptions.GmailSectionName);
+            var outlookOptions = _options.Get(EmailOptions.OutlookSectionName);
+
+            return Ok(new
+            {
+                GmailConfiguration = gmailOptions,
+                OutlookConfiguration = outlookOptions
+            });
+        }
+    }
+}
