a0b5ae5be97607256002cb72ff9e7756a92f9702
Yazan <yazan.dukhan@student.ap.be>
2024-09-30 19:13:03 +0200

    OefeningConfiguration DOEN!

---
+++ labos/labo05/OefeningConfiguration/OefeningConfiguration/ApplicationSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace OefeningConfiguration
+{
+    public class ApplicationSettings
+    {
+        public string AppName { get; set; } = string.Empty;
+        public string Version { get; set; } = string.Empty;
+        public int MaxUsers { get; set; }
+    }
+}
---
+++ labos/labo05/OefeningConfiguration/OefeningConfiguration/Controllers/ConfigurationController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+namespace OefeningConfiguration.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SettingsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public SettingsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        //Rechtstreeks lezen uit de appsettings.json
+        [HttpGet]
+        [Route("direct-from-appsettings")]
+        public ActionResult GetSettingsDirectly()
+        {
+            var appName = _configuration["ApplicationSettings:AppName"];
+            var version = _configuration["ApplicationSettings:Version"];
+            var maxUsers = _configuration["ApplicationSettings:MaxUsers"];
+
+            return Ok(new
+            {
+                AppName = appName,
+                Version = version,
+                MaxUsers = maxUsers
+            });
+        }
+
+        //Gebruiken van Configuration Binder.Bind()
+        [HttpGet]
+        [Route("using-binder")]
+        public ActionResult GetSettingsUsingBinder()
+        {
+            var appSettings = new ApplicationSettings();
+            _configuration.GetSection("ApplicationSettings").Bind(appSettings);
+
+            return Ok(new
+            {
+                appSettings.AppName,
+                appSettings.Version,
+                appSettings.MaxUsers
+            });
+        }
+    }
+}
