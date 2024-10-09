007afa6df9c8d120e81c94c2add83e025d64691d
Yazan <yazan.dukhan@student.ap.be>
2024-10-09 14:44:57 +0200

    added Oefening User Agent Middleware

--- labos/labo06/LoggingMetSeq/LoggingMetSeq/obj/Release/net8.0/LoggingMetSeq.AssemblyInfo.cs
+++ labos/labo06/LoggingMetSeq/LoggingMetSeq/obj/Release/net8.0/LoggingMetSeq.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoggingMetSeq")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f54d27f43678ea11ed763e39a431308146883ddd")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoggingMetSeq")]
